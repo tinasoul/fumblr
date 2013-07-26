111ec9c1313701260164fa7a35b4cc3a1df66b2b
Matt Buck <github@matthewbuck.com>
2013-07-26 06:57:10 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fumblr::Application.config.secret_token = 'def6b176e5f0c0cb63ffc8e5b6ba534094fa31aea9256f34faff5a66b51d44861e6834c3a6aac392927c4d3d3f252559c7b96fe823bd6df3febb832850a52a26'
